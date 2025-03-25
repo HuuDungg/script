9127688c8c236b219b2ff2b7f6f2c9ddfdec84bb
HuuDung <huudung@HuuDungs-MacBook-Pro.local>
2025-03-25 15:45:39 +0700

    fix

--- guide.js
+++ guide.js
@@ -1,15 +1,13 @@
-var obj = JSON.parse($response.body);
-
-obj= {
-  "status" : 0,
-  "products" : [
+// Không cần parse $response.body vì có thể nó undefined
+var obj = {
+  "status": 0,
+  "products": [
     {
-      "expirationDate" : "2090-01-01T00:00:00.000Z",
-      "expired" : false,
-      "productId" : "com.algoriddim.djay_iphone_free.pro.monthly"
+      "expirationDate": "2090-01-01T00:00:00.000Z",
+      "expired": false,
+      "productId": "com.algoriddim.djay_iphone_free.pro.monthly"
     }
   ]
 };
 
 $done({body: JSON.stringify(obj)});
-arrow
